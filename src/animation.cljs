99121c744b2ea083983957ddccfdbeac8dde1713
Jonas Östlund <ostlund.jobtech@gmail.com>
2025-12-14 16:43:56 +0100

    Camera model

--- src/animation.cljs
+++ src/animation.cljs
@@ -3,22 +3,46 @@
 (defn line-km [k m]
   {:k k :m m})
 
+(defn line-mapper [{:keys [k m]}]
+  (fn [x] (+ m (* k x))))
+
 (defn evaluate-line [{:keys [k m]} x]
   {:pre [(number? k)
          (number? m)
          (number? x)]}
   (+ (* k x) m))
 
-(defn ortho-camera [xmap ymap]
-  {:xmap xmap
-   :ymap ymap})
+(defn size? [x]
+  (and (vector? x)
+       (= 2 (count x))
+       (every? number? x)))
 
 (defn bounds [values]
-  [(apply min values)
-   (apply max values)])
+  {:lower (apply min values)
+   :upper (apply max values)})
 
 (defn bounding-box [points]
-  (let [vs (mapv bounds (apply map vector points))]
-    {:lower (mapv first vs)
-     :upper (mapv second vs)}))
+  (mapv bounds (apply map vector points)))
 
+(defn bd-scale [width {:keys [lower upper]}]
+  (/ width (- upper lower)))
+
+(defn bd-center [{:keys [lower upper]}]
+  (* 0.5 (+ lower upper)))
+
+(defn fit-line-with-scale [x y scale]
+  (line-km scale (- y (* scale x))))
+
+(defn fit-camera [[width height :as size]
+                  [xbd ybd :as _bbox]]
+  (let [scale (min (bd-scale width xbd)
+                   (bd-scale height ybd))]
+    {:size size
+     :xmap (fit-line-with-scale (bd-center xbd) (* 0.5 width) scale)
+     :ymap (fit-line-with-scale (bd-center ybd) (* 0.5 height) scale)}))
+
+(defn projector [{:keys [xmap ymap]}]
+  (let [xmapper (line-mapper xmap)
+        ymapper (line-mapper ymap)]
+    (fn [[x y & rest]]
+      (into [(xmapper x) (ymapper y)] rest))))
--- test/animation_test.cljs
+++ test/animation_test.cljs
@@ -3,6 +3,20 @@
             ["vitest" :refer [expect test]]))
 
 (test "animation"
-      (println (animation/bounding-box [[1 2]
-                                        [10 20]
-                                        [3 -4]])))
+      (-> (expect (animation/bounding-box [[1 2]
+                                           [10 20]
+                                           [3 -4]]))
+          (.toEqual [{:lower 1 :upper 10}
+                     {:lower -4 :upper 20}])))
+
+(test "fit-camera-test"
+      (let [pts [[1 1 3]
+                 [10 13 7]]
+            bdbox (animation/bounding-box pts)
+            size [640 480]
+            cam (animation/fit-camera size bdbox)
+            proj (animation/projector cam)]
+        (-> (expect (proj [1 1 3]))
+            (.toEqual [140 0 3]))
+        (-> (expect (proj [10 13 3 50]))
+            (.toEqual [500 480 3 50]))))
